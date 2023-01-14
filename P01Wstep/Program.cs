14689a124d6b05b0158945e8634d5945287806b3
kurs <kurs@DESKTOP-V3HDKPQ>
2023-01-14 11:08:00 +0100

    Add project files.

---
+++ P01Wstep/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P01Wstep
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string doPrzechowywaniaNapisow = "ala ma kota";
+
+            int zmiennaLiczbowa = 6;
+
+            Console.WriteLine(doPrzechowywaniaNapisow);
+
+            Console.ReadKey();
+            
+            // komentarz jedno wierszoy 
+
+            /* komentarz
+              wielo
+              wierszowy 
+             */
+
+        }
+    }
+}
---
+++ P02OperacjaNaNapisach/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P02OperacjaNaNapisach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string napis = "ala ma kota";
+                          //012345678910  
+            int dlugosc = napis.Length; // 11
+
+            // Console.WriteLine(dlugosc);
+            Console.WriteLine(dlugosc);
+
+            string s1 = napis.Substring(4); //ma kota
+            string s2 = napis.Substring(4, 2); //ma
+            //napis.Substring(4, 2);
+
+            Console.WriteLine(s1);
+
+            bool czyZawiera= napis.Contains("ma"); // true lub false 
+
+            bool typLogiczny = false;
+
+            string s3 = napis.ToUpper(); // zamiana na duze litery
+            string s4 = napis.ToLower();
+
+            // konkatencja napisów
+            string s5 = "ala" + "ma" + "kota";
+            // "alamakota"
+            string s6 = napis + " i psa";
+            // ala ma kota i psa
+
+            string s7 =  "ala".ToUpper(); //ALA
+
+            string s8 = "ala";
+            //s8 = s8.ToUpper(); 
+            s8.ToUpper(); // ten wynik nie jest do niczego przypisany
+            Console.WriteLine(s8);
+
+
+            string s9= napis.Replace("ma", "bedzie miała");
+            // ala bedzie miala kota
+
+            int pozycja= napis.IndexOf("ma"); //4
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
